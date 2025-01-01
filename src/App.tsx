4eaafb54765ef7f8968874138a85d55edbd305c5
umaxcode <maxwellodoom1729@gmail.com>
2025-01-01 16:37:50 +0000

    setup: project setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+function App() {
+  return (
+    <>
+      <Button className="bg-red-200 text-black" onClick={() => alert("DFDDF")}>
+        Click me
+      </Button>
+    </>
+  );
+}
+
+export default App;
