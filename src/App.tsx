c08a174af0a11c46f64f927d7e23e7a34b063db0
umaxcode <maxwellodoom1729@gmail.com>
2025-01-10 12:06:42 +0000

    add app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,30 @@
-import { Button } from "@/components/ui/button";
+import {
+  createBrowserRouter,
+  createRoutesFromChildren,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import AuthPage from "./pages/auth/AuthPage";
+import { LoginCallBack } from "./pages/auth/LoginCallBack";
+import { AdminPageLayout } from "./pages/admin/AdminPageLayout";
+import { UserPageLayout } from "./pages/user/UserPageLayout";
+import { ErrorPage } from "./pages/error/ErrorPage";
+
+const router = createBrowserRouter(
+  createRoutesFromChildren(
+    <Route>
+      <Route path="/" />
+      <Route index element={<AuthPage />} />
+      <Route path="/auth/callback" element={<LoginCallBack />} />
+      <Route path="/admin/dashboard" element={<AdminPageLayout />}></Route>
+      <Route path="/user/dashboard" element={<UserPageLayout />}></Route>
+      <Route path="*" element={<ErrorPage />} />
+    </Route>
+  )
+);
+
 function App() {
-  return (
-    <>
-      <Button className="bg-red-200 text-black" onClick={() => alert("DFDDF")}>
-        Click me
-      </Button>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
