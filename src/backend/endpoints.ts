d1945bab2acb778b3582ae5eeb958a8e6c01ea40
umaxcode <maxwellodoom1729@gmail.com>
2025-01-14 15:54:35 +0000

    fix: fix endpoint variable name

--- src/backend/endpoints.ts
+++ src/backend/endpoints.ts
@@ -1,6 +1,6 @@
 // Define the base URL for the API
 const AUTH_BASE_URL = import.meta.env.VITE_auth_endpoint;
-const APP_BASE_URL = import.meta.env.VITE_api_gateway_endpoin;
+const APP_BASE_URL = import.meta.env.VITE_api_gateway_endpoint;
 const clientId = import.meta.env.VITE_client_id;
 const callbackUrl = import.meta.env.VITE_callback_endpoint;
 
