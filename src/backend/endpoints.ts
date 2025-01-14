1cbb9485010d8e8f5254ed432fc006882af8ac09
umaxcode <maxwellodoom1729@gmail.com>
2025-01-14 08:51:40 +0000

    update endpoints

---
+++ src/backend/endpoints.ts
@@ -0,0 +1,16 @@
+// Define the base URL for the API
+const AUTH_BASE_URL = import.meta.env.VITE_auth_endpoint;
+const APP_BASE_URL = import.meta.env.VITE_api_gateway_endpoin;
+const clientId = import.meta.env.VITE_client_id;
+const callbackUrl = import.meta.env.VITE_callback_endpoint;
+
+// Exported endpoints
+export const Endpoints = {
+  AUTH: {
+    LOGIN: `${AUTH_BASE_URL}/login?client_id=${clientId}&response_type=code&redirect_uri=${callbackUrl}`,
+    TOKEN: `${AUTH_BASE_URL}/oauth2/token`,
+    CALLBACK: callbackUrl,
+  },
+  USERS: `${APP_BASE_URL}/users`,
+  TASKS: `${APP_BASE_URL}/tasks`,
+};
