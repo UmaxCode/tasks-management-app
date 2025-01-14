0b83d39d1d8d26e5082ac3e8f9eaaec7fe3218c2
umaxcode <maxwellodoom1729@gmail.com>
2025-01-14 15:56:08 +0000

    feat: update applicaton paths

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ import { LoginCallBack } from "./pages/auth/LoginCallBack";
 import { AdminPageLayout } from "./pages/admin/AdminPageLayout";
 import { UserPageLayout } from "./pages/user/UserPageLayout";
 import { ErrorPage } from "./pages/error/ErrorPage";
+import { UsersPage } from "./pages/admin/UsersPage";
+import { TasksPage } from "./pages/admin/TasksPage";
+import UserDetailsPage from "./pages/admin/UserDetailsPage";
 
 const router = createBrowserRouter(
   createRoutesFromChildren(
@@ -16,7 +19,11 @@ const router = createBrowserRouter(
       <Route path="/" />
       <Route index element={<AuthPage />} />
       <Route path="/auth/callback" element={<LoginCallBack />} />
-      <Route path="/admin/dashboard" element={<AdminPageLayout />}></Route>
+      <Route path="/admin/dashboard" element={<AdminPageLayout />}>
+        <Route index element={<UsersPage />} />
+        <Route path="users/:email" element={<UserDetailsPage />} />
+        <Route path="tasks" element={<TasksPage />} />
+      </Route>
       <Route path="/user/dashboard" element={<UserPageLayout />}></Route>
       <Route path="*" element={<ErrorPage />} />
     </Route>
